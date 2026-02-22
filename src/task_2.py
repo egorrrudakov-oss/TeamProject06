c72eefa1b462e8f7d2d8c5b867af30d93d995c9b
Staish69Batareyka <anastaish.batareyka@yandex.ru>
2026-02-22 18:03:54 +0300

    feat(task_2): Реализована проверка наличия папки visits и вывод списка дат

--- src/task_2.py
+++ src/task_2.py
@@ -6,6 +6,21 @@ full_name = input('Enter the full name of the patient separated by space (Ivanov
 capitalize = '_'.join([name.capitalize() for name in full_name.split()])
 
 if os.path.isdir(f'patients/{capitalize}'):
+
+    # Проверка на наличие папки visits
+    try:
+        visits = f'patients/{capitalize}/visits'
+        if os.listdir(visits):
+            print('Previous doctor appointments:')
+            for i in os.listdir(visits):
+                print(i[:-4])
+        else:
+            print('This is the first appointment with the doctor!')
+            exit() # выход из программы
+    except FileNotFoundError:
+        print('This is the first appointment with the doctor!')
+        exit()
+
     date = input('Enter the date of the appointment you want to see: ')
     try:
         f = open(f'patients/{capitalize}/visits/{date}.txt', 'r', encoding='utf-8')
@@ -13,10 +28,10 @@ if os.path.isdir(f'patients/{capitalize}'):
         f.close()
     except FileNotFoundError:
         print('There was no such appointment with the doctor!')
-
 else:
     print('There is no such patient!')
     exit() # по условию жирным шрифтом было написано ЗАВЕРШИТЬ ПРОГРАММУ
 
 # Ivanov ivan ivanovich
 # 2024-09-05
+# Sidorova Svetlana Sergeevna
